ef36a49497b714a3e2504fad29929e3fec20dbb2
cexll <evanxian9@gmail.com>
2021-02-01 10:52:00 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是goblog!</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记,如你有反馈或建议,请联系 " +
-		 "<a href=\"mailto:evanxian9@gmail.com\">evanxian9@gmail.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :( </h1>" +
@@ -19,7 +16,27 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记,如你有反馈或建议,请联系 " +
+		 "<a href=\"mailto:evanxian9@gmail.com\">evanxian9@gmail.com</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "访问文章列表")
+		case "POST":
+			fmt.Fprint(w, "创建新的文章")
+		}
+		//id := strings.SplitN(r.URL.Path, "/", 3)[2]
+		//fmt.Fprint(w, "文章 ID:" + id)
+	})
+
+
+	http.ListenAndServe(":3000", router)
 }
