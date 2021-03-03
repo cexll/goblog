c2223ed518535feff3e3ba92d7824d1978456d9e
cexll <evanxian9@gmail.com>
2021-03-03 16:51:15 +0800

    创建用户

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,17 +1,22 @@
 package bootstrap
 
 import (
-	"goblog/pkg/model"
 	"time"
+
+	"goblog/app/models/article"
+	"goblog/app/models/user"
+	"goblog/pkg/model"
+
+	"gorm.io/gorm"
 )
 
-func SetupDB()  {
+func SetupDB() {
 
 	// 建立数据库连接池
 	db := model.ConnectDB()
 
 	// 命令打印数据库请求的信息
-	sqlDB,_ := db.DB()
+	sqlDB, _ := db.DB()
 
 	// 设置最大连接数
 	sqlDB.SetMaxOpenConns(100)
@@ -19,5 +24,14 @@ func SetupDB()  {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// 创建和维护数据表结构
+	migration(db)
+}
 
+func migration(db *gorm.DB) {
+	// 自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
