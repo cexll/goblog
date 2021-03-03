4b3e78f3e2ef25fa6d845b5bc1ebd853962e7d6f
cexll <evanxian9@gmail.com>
2021-03-03 10:22:51 +0800

    重构更新文章

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -24,10 +24,20 @@ func GetAll() ([]Article, error) {
 	return articles, nil
 }
 
-func (article *Article) Create() (err error)  {
+func (article *Article) Create() (err error) {
 	if err = model.DB.Create(&article).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
 	return nil
+}
+
+func (article *Article) Update() (rowsAffected int64, err error) {
+	result := model.DB.Save(&article)
+	if err = result.Error; err != nil {
+		logger.LogError(err)
+		return 0, err
+	}
+
+	return result.RowsAffected, nil
 }
--- routes/web.go
+++ routes/web.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 
 	"goblog/app/http/controllers"
+
 	"github.com/gorilla/mux"
 )
 
-func RegisterWebRoutes(r *mux.Router)  {
+func RegisterWebRoutes(r *mux.Router) {
 	// 静态页面
 	pc := new(controllers.PagesController)
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
@@ -19,5 +20,7 @@ func RegisterWebRoutes(r *mux.Router)  {
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 	r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
 	r.HandleFunc("/articles/create", ac.Create).Methods("GET").Name("articles.create")
-	//r.HandleFunc("/articles", ac.Store).Methods("POST").Name("articles.store")
+	r.HandleFunc("/articles", ac.Store).Methods("POST").Name("articles.store")
+	r.HandleFunc("/articles/{id:[0-9]+}/edit", ac.Edit).Methods("GET").Name("articles.edit")
+	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 }
